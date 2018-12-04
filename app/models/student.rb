42db716117316ab1217bbbbb44f35f681c60b8c3
SiamKing <thesiameseelectric@gmail.com>
2018-12-03 20:12:10 -0800

    Im partial

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,12 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(name)
+    students = Student.all
+    matches = students.select do |student|
+      student.name.downcase.include?(name)
+    end
+    matches.empty? ? students : matches
+  end
 end
